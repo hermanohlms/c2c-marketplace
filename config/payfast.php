26dc52387e54c80ba84c3fd96b9f5560110376a2
hermanohlms <hermanohlms8722@gmail.com>
2026-06-18 22:51:20 +0200

    PayFast Check

--- config/payfast.php
+++ config/payfast.php
@@ -4,19 +4,19 @@ require_once __DIR__ . '/env.php';
 
 $sandbox = getenv('PAYFAST_SANDBOX') === 'true';
 
+$appUrl = strtolower(rtrim(getenv('APP_URL'), '/'));
+
 return [
     'merchant_id' => getenv('PAYFAST_MERCHANT_ID'),
     'merchant_key' => getenv('PAYFAST_MERCHANT_KEY'),
     'passphrase' => getenv('PAYFAST_PASSPHRASE'),
 
-    'sandbox' => $sandbox,
+    'sandbox' => getenv('PAYFAST_SANDBOX') === 'true',
 
     'sandbox_url' => 'https://sandbox.payfast.co.za/eng/process',
     'live_url' => 'https://www.payfast.co.za/eng/process',
 
-    'app_url' => rtrim(getenv('APP_URL'), '/'),
-
-    'return_url' => rtrim(getenv('APP_URL'), '/') . '/index.php?page=payment-success',
-    'cancel_url' => rtrim(getenv('APP_URL'), '/') . '/index.php?page=payment-cancelled',
-    'notify_url' => rtrim(getenv('APP_URL'), '/') . '/index.php?page=payfast-itn'
+    'return_url' => $appUrl . '/index.php?page=payment-success',
+    'cancel_url' => $appUrl . '/index.php?page=payment-cancelled',
+    'notify_url' => $appUrl . '/index.php?page=payfast-itn'
 ];
